2d0f0ce048199e2c1372e61226358cc2943ef2d6
lethanhtung <lethanhtung@localhost>
2023-09-25 09:29:41 +0700

    tỳutyftygygh

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module laptrinhcoban2 {
+}
---
+++ src/tuan1/BaiLuyenTap.java
@@ -0,0 +1,11 @@
+package tuan1;
+
+public class BaiLuyenTap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HI ");
+
+	}
+
+}
